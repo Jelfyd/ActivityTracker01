0ea1e03ff4752fc7ab601c3b61cb625c7f773217
Babych Andrey <babichandrey79@gmail.com>
2025-12-09 09:35:17 +0200

    Переобив тести

--- ActivityTracker.Core/obj/Debug/net10.0/ActivityTracker.Core.AssemblyInfo.cs
+++ ActivityTracker.Core/obj/Debug/net10.0/ActivityTracker.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ActivityTracker.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29eb7e4d9dac3664af336bdc57d99e4d99fc5e22")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e58da9b2d4a84aed4f942338ff69d8ccba1b171")]
 [assembly: System.Reflection.AssemblyProductAttribute("ActivityTracker.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ActivityTracker.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ActivityTracker.Test/UnitTest1.cs
+++ ActivityTracker.Test/UnitTest1.cs
@@ -1,73 +1,80 @@
-﻿namespace ActivityTracker.Tests;
+﻿namespace ActivittyTracker.Test;
+using Xunit;
 using ActivityTracker.Core;
-using NUnit.Framework;
-public class UnitTest1
+
+
+public class ActivityTrackerTests
 {
-    [Test]
+    [Fact]
     public void Constructor_ShouldThrow_WhenDailyGoalIsNegative()
     {
         Assert.Throws<ArgumentException>(() => new ActivityTracker(-5));
     }
 
-    [Test]
+    [Fact]
     public void Constructor_ShouldSetDailyGoal()
     {
         var tracker = new ActivityTracker(5000);
-        Assert.That(tracker.DailyGoal, Is.EqualTo(5000));
+        Assert.Equal(5000, tracker.DailyGoal);
     }
 
-    [Test]
+    [Fact]
     public void AddSteps_ShouldIncreaseSteps_WhenPositive()
     {
         var t = new ActivityTracker(5000);
         t.AddSteps(1000);
-        Assert.That(t.StepsToday, Is.EqualTo(1000));
+        Assert.Equal(1000, t.StepsToday);
     }
 
-    [Test]
+    [Fact]
     public void AddSteps_ShouldIgnoreNegativeValues()
     {
         var t = new ActivityTracker(5000);
         t.AddSteps(-300);
-        Assert.That(t.StepsToday, Is.EqualTo(0));
+        Assert.Equal(0, t.StepsToday);
     }
-    [Test]
+
+    [Fact]
     public void ResetDay_ShouldSetStepsToZero()
     {
         var t = new ActivityTracker(5000);
         t.AddSteps(2000);
         t.ResetDay();
-        Assert.That(t.StepsToday, Is.EqualTo(0));
+        Assert.Equal(0, t.StepsToday);
     }
-    [Test]
+
+    [Fact]
     public void GetProgress_ShouldReturn100_WhenGoalIsZero()
     {
         var t = new ActivityTracker(0);
-        Assert.That(t.GetProgress(), Is.EqualTo(100));
+        Assert.Equal(100, t.GetProgress());
     }
 
-    [Test]
-    public void GetProgress_ShouldReturnCorrectPercentage()
+    [Theory]
+    [InlineData(4000, 1000, 25)]
+    [InlineData(4000, 555, 13)]
+    public void GetProgress_ShouldReturnCorrectPercentage(int dailyGoal, int steps, int expected)
     {
-        var t = new ActivityTracker(4000);
-        t.AddSteps(1000);
-        Assert.That(t.GetProgress, Is.EqualTo(25));
+        var t = new ActivityTracker(dailyGoal);
+        t.AddSteps(steps);
+        Assert.Equal(expected, t.GetProgress());
     }
-    [Test]
+
+    [Fact]
     public void IsGoalReached_ShouldBeTrue_WhenStepsEnough()
     {
         var t = new ActivityTracker(3000);
         t.AddSteps(3000);
-        Assert.That(t.IsGoalReached, Is.True);
+        Assert.True(t.IsGoalReached);
     }
 
-    [Test]
+    [Fact]
     public void IsGoalReached_ShouldBeFalse_WhenStepsNotEnough()
     {
         var t = new ActivityTracker(3000);
         t.AddSteps(1000);
-        Assert.That(t.IsGoalReached, Is.False);
+        Assert.False(t.IsGoalReached);
     }
-
-
 }
+
+
